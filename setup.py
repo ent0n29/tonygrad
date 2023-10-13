70076b1ad39b0e1f320e4e7d077c26434787f0bc
ent0n29 <[stanoantonio29@gmail.com]>
2023-10-13 16:18:34 +0200

    register tonygrad as a package

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tonygrad",
+    version="1.0.0",
+    author="Antonio Stano",
+    author_email="stanoantonio29@gmail.com",
+    description="A tiny scalar-valued autograd engine with a small PyTorch-like neural network library on top.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ent0n29/micrograd",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+
+)
