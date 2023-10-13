c20a7a94aea5af4f50a725ed35059a07c4b3521b
ent0n29 <[stanoantonio29@gmail.com]>
2023-10-13 15:46:37 +0200

    fixed bugs, added some operations in the engine, fixed the nn library

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,54 +1,50 @@
-import math
-import numpy as np
-import matplotlib.pyplot as plt
+import math 
 
 class Value:
-    def __init__(self, data, _children=(), _op='', label=''): #children->empty tuple
+  
+    def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
         self.grad = 0.0
-        self._backward = lambda: None #by default it does nothing
-        self._prev = set(_children) #in the class it will be a set
+        self._backward = lambda: None
+        self._prev = set(_children)
         self._op = _op
         self.label = label
 
     def __repr__(self):
         return f"Value(data={self.data})"
-    
+  
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
-
-        # in add the gradient distrubution flows equally to its childs
+    
         def _backward():
-            self.grad += 1.0 * out.grad 
+            self.grad += 1.0 * out.grad
             other.grad += 1.0 * out.grad
-        out._backward = _backward #function that propagates the gradient
-
-        return out
+        out._backward = _backward
     
+        return out
+
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
-        
-        #in mul we use the chain rule of derivatives
+    
         def _backward():
-            self.grad += other.data * out.grad  #chain rule!
-            other.grad += self.data * out.grad  #chain rule!
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+      
+        return out
+  
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        out = Value(self.data**other, (self,), f'**{other}')
+
+        def _backward():
+            self.grad += other * (self.data ** (other - 1)) * out.grad
         out._backward = _backward
 
         return out
-
-    #using tanh function to "squish" the output
-    def tanh(self):
-        x = self.data
-        t = (math.exp(2*x)-1)/(math.exp(2*x)+1)
-        out = Value(t, (self, ), 'tanh') #just one child, self, a tuple
-    
-        def _backward():
-            #derivative of tanh chain ruled to out.grad into self.grad
-            self.grad += (1 - t**2) * out.grad
-        out._backward = _backward
-        
-        return out
-    
+  
     def __rmul__(self, other): # other * self
         return self * other
 
@@ -63,21 +59,33 @@ class Value:
 
     def __radd__(self, other): # other + self
         return self + other
+
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
+        out = Value(t, (self, ), 'tanh')
     
+        def _backward():
+            self.grad += (1 - t**2) * out.grad
+        out._backward = _backward
+    
+        return out
+  
     def exp(self):
         x = self.data
         out = Value(math.exp(x), (self, ), 'exp')
     
         def _backward():
             self.grad += out.data * out.grad
-            out._backward = _backward
+        out._backward = _backward
     
         return out
-
+  
+  
     def backward(self):
-
+    
         topo = []
-        visited = set() 
+        visited = set()
         def build_topo(v):
             if v not in visited:
                 visited.add(v)
@@ -85,7 +93,7 @@ class Value:
                 build_topo(child)
             topo.append(v)
         build_topo(self)
-
+    
         self.grad = 1.0
         for node in reversed(topo):
             node._backward()
