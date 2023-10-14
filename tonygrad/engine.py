b82cf829d5a63bc77c1b1d868a6f655261ab0a0d
ent0n29 <[stanoantonio29@gmail.com]>
2023-10-14 12:10:14 +0200

    update engine, nn, add graph drawing inside tonygrad, add reLU activation function

--- tonygrad/engine.py
+++ tonygrad/engine.py
@@ -1,65 +1,62 @@
-import math 
+import math
 
 class Value:
-  
+
+    """ stores a single scalar value and its gradient """
+
     def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
-        self.grad = 0.0
+        self.grad = 0
         self._backward = lambda: None
         self._prev = set(_children)
-        self._op = _op
+        self._op = _op 
         self.label = label
 
-    def __repr__(self):
-        return f"Value(data={self.data})"
-  
+    # defining various operations
+
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
-    
+
         def _backward():
-            self.grad += 1.0 * out.grad
-            other.grad += 1.0 * out.grad
+            self.grad += out.grad
+            other.grad += out.grad
         out._backward = _backward
-    
+
         return out
 
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
-    
+
         def _backward():
             self.grad += other.data * out.grad
             other.grad += self.data * out.grad
         out._backward = _backward
-      
+
         return out
-  
+
     def __pow__(self, other):
         assert isinstance(other, (int, float)), "only supporting int/float powers for now"
         out = Value(self.data**other, (self,), f'**{other}')
 
         def _backward():
-            self.grad += other * (self.data ** (other - 1)) * out.grad
+            self.grad += (other * self.data**(other-1)) * out.grad
         out._backward = _backward
 
         return out
-  
-    def __rmul__(self, other): # other * self
-        return self * other
+    
+    # functions to "squish" the output, reLU and tanh
 
-    def __truediv__(self, other): # self / other
-        return self * other**-1
+    def relu(self):
+        out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
 
-    def __neg__(self): # -self
-        return self * -1
+        def _backward():
+            self.grad += (out.data > 0) * out.grad
+        out._backward = _backward
 
-    def __sub__(self, other): # self - other
-        return self + (-other)
-
-    def __radd__(self, other): # other + self
-        return self + other
-
+        return out
+    
     def tanh(self):
         x = self.data
         t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
@@ -70,30 +67,51 @@ class Value:
         out._backward = _backward
     
         return out
-  
-    def exp(self):
-        x = self.data
-        out = Value(math.exp(x), (self, ), 'exp')
     
-        def _backward():
-            self.grad += out.data * out.grad
-        out._backward = _backward
-    
-        return out
-  
-  
+    # backpropagation with a reverse topological sort on the built graph
+
     def backward(self):
-    
+
+        # topological order all of the children in the graph
         topo = []
         visited = set()
         def build_topo(v):
             if v not in visited:
                 visited.add(v)
-            for child in v._prev:
-                build_topo(child)
-            topo.append(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
         build_topo(self)
+
+        # go one variable at a time and apply the chain rule to get its gradient
+        self.grad = 1
+        for v in reversed(topo):
+            v._backward()
+
+    # other operations
+
+    def __neg__(self): # -self
+        return self * -1
+
+    def __radd__(self, other): # other + self
+        return self + other
+
+    def __sub__(self, other): # self - other
+        return self + (-other)
+
+    def __rsub__(self, other): # other - self
+        return other + (-self)
+
+    def __rmul__(self, other): # other * self
+        return self * other
+
+    def __truediv__(self, other): # self / other
+        return self * other**-1
+
+    def __rtruediv__(self, other): # other / self
+        return other * self**-1
     
-        self.grad = 1.0
-        for node in reversed(topo):
-            node._backward()
+    # print the object with attributes value(data) and its gradient
+
+    def __repr__(self):
+        return f"Value(data={self.data}, grad={self.grad})"
--- tonygrad/nn.py
+++ tonygrad/nn.py
@@ -1,44 +1,62 @@
 import random
 from tonygrad.engine import Value
 
+class Module:
 
-class Neuron:
-  
-  def __init__(self, nin):
-    self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
-    self.b = Value(random.uniform(-1,1))
-  
-  def __call__(self, x):
-    # w * x + b
-    act = sum((wi*xi for wi, xi in zip(self.w, x)), self.b)
-    out = act.tanh()
-    return out
-  
-  def parameters(self):
-    return self.w + [self.b]
+    # zero-grad method to set the model grad to 0, before doing backpropagation 
 
-class Layer:
-  
-  def __init__(self, nin, nout):
-    self.neurons = [Neuron(nin) for _ in range(nout)]
-  
-  def __call__(self, x):
-    outs = [n(x) for n in self.neurons]
-    return outs[0] if len(outs) == 1 else outs
-  
-  def parameters(self):
-    return [p for neuron in self.neurons for p in neuron.parameters()]
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0
 
-class MLP:
-  
-  def __init__(self, nin, nouts):
-    sz = [nin] + nouts
-    self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
-  
-  def __call__(self, x):
-    for layer in self.layers:
-      x = layer(x)
-    return x
-  
-  def parameters(self):
-    return [p for layer in self.layers for p in layer.parameters()]
+    def parameters(self):
+        return []
+
+class Neuron(Module):
+
+    def __init__(self, nin, nonlin=True):
+        self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
+        self.b = Value(0)
+        self.nonlin = nonlin
+
+    def __call__(self, x):
+        act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)
+        return act.relu() if self.nonlin else act
+
+    def parameters(self):
+        return self.w + [self.b]
+
+    def __repr__(self):
+        return f"{'ReLU' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
+
+class Layer(Module):
+
+    def __init__(self, nin, nout, **kwargs):
+        self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
+
+    def __call__(self, x):
+        out = [n(x) for n in self.neurons]
+        return out[0] if len(out) == 1 else out
+
+    def parameters(self):
+        return [p for n in self.neurons for p in n.parameters()]
+
+    def __repr__(self):
+        return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
+
+class MLP(Module):
+
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+1], nonlin=i!=len(nouts)-1) for i in range(len(nouts))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+
+    def __repr__(self):
+        return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
