6d724cf4d1fc68bcf974e9c46fadfcda78fad6db
ent0n29 <[stanoantonio29@gmail.com]>
2023-10-14 14:30:56 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tonygrad",
-    version="1.1.1",
+    version="1.1.2",
     author="Antonio Stano",
     author_email="stanoantonio29@gmail.com",
     description="A tiny scalar-valued autograd engine with a small PyTorch-like neural network library on top.",
